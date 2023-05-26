cea4cb7e9d0ffddfd972f7ae9ae4642cdbeb54dd
riskianandaa <Risananda999@gmail.com>
2023-05-26 14:05:26 +0700

    add: modul product

--- js/script.js
+++ js/script.js
@@ -1,24 +1,75 @@
-// Toggle class active
-
+// Toggle class active hamburger menu
 const navbarNav = document.querySelector
 
 ('.navbar-nav');
 
 // Ketika hamburger menu di klik
-
 document.querySelector('#hamburger-menu').
-onclick = () => {
+onclick = (e) => {
     navbarNav.classList.toggle('active')
+    e.preventDefault()
 }
 
-// Klik di luar sidebar untuk menghilangkan nav
+// Toggle class active search from
+const searchForm = document.querySelector('.search-form')
+const searchBox =document.querySelector('#search-box')
 
-const hamburger = document.querySelector
+document.querySelector('#search-button').
+onclick = (e) => {
+    searchForm.classList.toggle('active')
+    searchBox.focus()
+    e.preventDefault()
+}
 
-('#hamburger-menu')
+// Toggle class active shopping cart
+const shoppingCart = document.querySelector('.shopping-cart')
+
+document.querySelector('#shopping-cart-button').
+onclick = (e) => {
+    shoppingCart.classList.toggle('active')
+    e.preventDefault()
+}
+
+// Klik di luar element
+
+const hamburger = document.querySelector('#hamburger-menu')
+const searchButton = document.querySelector('#search-button')
+const shoppingCartButton = document.querySelector('#shopping-cart-button')
 
 document.addEventListener('click', function(e) {
     if(!hamburger.contains(e.target) && !navbarNav.contains(e.target)){
         navbarNav.classList.remove('active') 
     }
+    if(!searchButton.contains(e.target) && !searchForm.contains(e.target)){
+        searchForm.classList.remove('active') 
+    }
+    if(!shoppingCartButton.contains(e.target) && !shoppingCart.contains(e.target)){
+        shoppingCart.classList.remove('active') 
+    }
 })
+
+// modal box
+const itemDetailModal = document.querySelector('#item-detail-modal')
+const itemDetailButtons = document.querySelectorAll('.item-detail-button')
+
+itemDetailButtons.forEach((btn) => {
+    btn.onclick = (e) => {
+        itemDetailModal.style.display = 'flex'
+        e.preventDefault()
+    }
+})
+
+// klik tombol close modal
+
+document.querySelector('.modal .close-icon').onclick = (e) => {
+    itemDetailModal.style.display = 'none'
+    e.preventDefault()
+}
+
+// klik di laur modal
+window.onclick = (e) => {
+    if(e.target === itemDetailModal) {
+        itemDetailModal.style.display = 'none'
+    }
+}
+
